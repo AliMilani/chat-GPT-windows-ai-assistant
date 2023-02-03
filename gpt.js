8575d3fac433f3b54629653a69d7db3978765ad2
Ali Milani <59066012+AliMilani@users.noreply.github.com>
2023-02-03 23:33:03 +0330

    update

--- gpt.js
+++ gpt.js
@@ -1,7 +1,5 @@
 const { ModuleImporter } = require("@humanwhocodes/module-importer");
 
-const dotenv = require("dotenv");
-dotenv.config();
 const lastQuestions = [];
 let lastConversation = null;
 
@@ -16,31 +14,16 @@ async function getAPI() {
    */
   const { ChatGPTAPI } = await importer.import("chatgpt");
   const api = new ChatGPTAPI({
-    apiKey: process.env.OPENAI_API_KEY,
-    // debug: true,
+	  apiKey: process.env.OPENAI_API_KEY,
   });
   return api;
 }
 
-/**
- * takes a string, a key, and a value, and returns the string with the key replaced by
- * the value.
- * @param str - The string to be replaced.
- * @param key - The key to replace
- * @param value - The value to be replaced
- * @returns a string that has been replaced with the value of the key.
- */
 const _replaceKey = (str, key, value) => {
   return str.replace(key, value);
 };
 
-/**
- * It takes a question and a list of the last questions asked, and generates a text file with the
- * question and the last questions.
- * @param question - "What is your name?"
- * @param lastQuestions - An array of strings, each string is a question that was asked in the last
- * questions.
- */
+
 const generateAskPrompt = async (question, lastQuestions) => {
   const promptFileName = "input-prompt.txt";
   const promptText = require("fs").readFileSync(promptFileName, "utf8");
@@ -51,20 +34,17 @@ const generateAskPrompt = async (question, lastQuestions) => {
     lastQuestionsKey,
     lastQuestions.join("\n")
   );
-  prompt = _replaceKey(prompt, inputQuestionKey, question);
-  //   console.log(prompt);
+	prompt = _replaceKey(prompt, inputQuestionKey, question);
   return prompt;
 };
 
 const askQuestion = async (question) => {
   const api = await getAPI();
-  const prompt = await generateAskPrompt(question, lastQuestions);
-  require("fs").writeFileSync("input-prompt-to-send.txt", prompt, "utf8");
+	const prompt = await generateAskPrompt(question, lastQuestions);
   const gptAPIOpts = lastConversation
     ? { conversationId: lastConversation }
     : {};
-  const res = await api.sendMessage(prompt, gptAPIOpts);
-  console.log(`res.conversationId: ${res.conversationId}`);
+	const res = await api.sendMessage(prompt, gptAPIOpts);
   const { text, conversationId } = res;
   lastQuestions.push(text);
   lastConversation = conversationId;
--- index.js
+++ index.js
@@ -8,23 +8,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.get("/ask", async (req, res) => {
-	/* example:
-	 http://127.0.0.1:5000/ask?question=play jazz music made by Miles Davis
-	 [
-		{
-			$question: "play jazz music made by Miles Davis",
-			$answer: {
-			answer_text: "Playing jazz music made by Miles Davis on Spotify.",
-			action: "spotify_play_artist",
-			action_data: "Miles Davis",
-			},
-		},
-	]
-	*/
 	const question = req.query.question;
-	console.log(req.query);
 	const answer = await askQuestion(question);
-	console.log(answer);
 	res.send(answer);
 	});
 
